2c5b88f23912e441870c55aa6619edc794637666
Norah (Jo) Vii <70073104+norahvii@users.noreply.github.com>
2023-12-13 09:26:54 -0600

    Update giveinfo.py

--- giveinfo.py
+++ giveinfo.py
@@ -2,18 +2,11 @@ import os
 import pandas as pd
 
 def process_csv_file(file_name):
-    # read file
-    df = pd.read_csv(file_name)
-    df = df.dropna()
-
-    labels_str = ', '.join(map(str, set(df['brain_region'])))
+    df = pd.read_csv(file_name).dropna()
+    labels_str = ', '.join(map(str, df['brain_region'].unique()))
     print(f"File: {file_name}\nLabels: {labels_str}")
-    stain_str = ', '.join(map(str, set(df['stain_id'])))
+    stain_str = ', '.join(map(str, df['stain_id'].unique()))
     print(f"Stains: {stain_str}\n")
 
-# Get a list of all CSV files in the current working directory
 csv_files = [file for file in os.listdir() if file.endswith('.csv')]
-
-# Iterate through each CSV file and process it
-for file_name in csv_files:
-    process_csv_file(file_name)
+list(map(process_csv_file, csv_files))
